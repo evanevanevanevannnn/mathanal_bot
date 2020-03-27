443ae793bc59a70243b76f2fbb130a0a0b46eafb
Ivan Sukhov <30556221+sukhov-ivan@users.noreply.github.com>
2020-03-28 01:38:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-import time
-import vk_api
+import time, vk_api, csv, requests, io, sys
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import csv
-import requests
-import io
+
+#---------------константы------------------
 
 Token = '55231cb8b6506f19461ce9d469269f702a28b2f6552e3e290eea74a5fb30bfc27a71b728cb9fca9a4c871'
-Peer_id = 2000000001
+Peer_id = 2000000002
+
 vk_map = {
     510092998: 'дмитриенков',
     265757761: 'магомедов',
@@ -36,22 +35,59 @@ vk_map = {
     369807714: 'сухов'
 }
 
+admins = [
+    'сухов'
+]
+
+vk_session = vk_api.VkApi(token=Token)
+longpoll = VkBotLongPoll(vk_session, 164613215)
+vk = vk_session.get_api()
+
+solved_problems = dict()
+solved_something = set()
+_106 = dict()
+
+#-------------вспомогательные функции------------------
+
+def find_id(name):
+    for _id in vk_map:
+        if (vk_map[_id] == name):
+            return _id
+    return 0
+
 def safe_int(string):
     try:
         return int(string)
     except:
         return 0
 
-def dict_to_str(d):
+def otformatirovat(l):
+    string = str(l)
+    string = string.replace('[', '')
+    string = string.replace(']', '')
+    string = string.replace("'", "")
+
+    return string
+
+def otformatirovat2(d):
     string = str(d)
     string = string.replace('{', '')
     string = string.replace('}', '')
-    string = string.replace("'", '')
-    string = string.replace('[', '')
-    string = string.replace(']', '')
+    string = string.replace("'", "")
     string = string.replace(', ', '\n')
 
     return string
+
+def seems_like_problem(problem):
+    return True
+
+def sorted_by_key(d):
+    res = dict()
+
+    for item in sorted(d.items(), key=lambda x: -x[1]):
+        res[item[0]] = item[1]
+
+    return res
 
 def read_106():
     file_id = '1JzzFqaHb04bA_w_V8RJNT2YdOhCCfQUtggZlZU4mqHc'
@@ -62,8 +98,7 @@ def read_106():
 
     reader = csv.reader(sio, dialect=csv.excel)
 
-    _106 = dict()
-
+    unsorted_106 = dict()
     for row in reader:
 
         if (row[0] == '' or row[0] == 'ФИО'):
@@ -72,77 +107,187 @@ def read_106():
         name = row[0].lower().split(' ')[0]
         points = sum(list(map(safe_int, row[1:])))
 
-        _106[name] = points
+        unsorted_106[name] = points
 
-    return _106
+    return sorted_by_key(unsorted_106)
 
-vk_session = vk_api.VkApi(token=Token)
-longpoll = VkBotLongPoll(vk_session, 164613215)
-vk = vk_session.get_api()
+def read_solved_problems():
+    global solved_problems
 
-solved_problems = dict()
-solved_something = set()
+    f = open('solved_problems.txt', 'r')
+
+    for line in f:
+        solved_problems[line.split(':')[0]] = line.split(':')[1][1:].split(',')
+
+def write_solved_problems():
+    f = open('solved_problems.txt', 'w')
+
+    for problem in solved_problems:
+        line = problem + ':'
+        for solution in solved_problems[problem]:
+            line += ',' + solution
+
+    line += '\n'
+    f.write(line)
+
+#---------------команды бота-------------------
+
+def restart(message):
+    global solved_problems, solved_something, _106
+
+    if (vk_map[message['from_id']] in admins):
+        solved_problems = dict()
+        solved_something = set()
+        _106 = read_106()
+
+def solved(message, forced=False):
+    problems = [i for i in message['text'].split(' ') if len(i) != 0][1:]
+    solver = vk_map[message['from_id']]
+    not_problems = list()
+
+    for problem in problems:
+        if not (seems_like_problem(problem) or forced):
+            not_problems.append(problem)
+            continue
+
+        if (problem not in solved_problems):
+            solved_problems[problem] = list()
+        if (solver not in solved_problems[problem]):
+            solved_problems[problem].append(solver)
+
+    if (len(not_problems) != 0):
+        message_text = 'данные номера: ' + otformatirovat(not_problems) + ' выглядят так, будто вы описались. если это не так используйте \\force_solved.'
+        vk.messages.send(peer_id=Peer_id, message=message_text, random_id=time.time())
+        time.sleep(1)
+
+    write_solved_problems()
+
+def not_solved(message):
+    problems = [i for i in message['text'].split(' ') if len(i) != 0][1:]
+    solver = vk_map[message['from_id']]
+    not_solved = list()
+
+    for problem in problems:
+        if (problem in solved_problems):
+            solved_problems[problem].remove(solver)
+
+            if (len(solved_problems[problem]) == 0):
+                solved_problems.pop(problem)
+        else:
+            not_solved.append(problem)
+
+    if (len(not_solved) != 0):
+        message_text = 'не уверен, что вы решили следующие задачки: ' + otformatirovat(not_solved) + '. проверьте не описались ли вы))'
+        vk.messages.send(peer_id=Peer_id, message=message_text, random_id=time.time())
+        time.sleep(1)
+
+def increase_priority(message):
+    global _106
+
+    solvers = [i for i in message['text'].split(' ') if len(i) != 0][1:]
+    not_in_106 = list()
+
+    for solver in solvers:
+        if (solver.lower() in _106):
+            _106[solver.lower()] = -1
+        else:
+            not_in_106.append(solver.lower())
+
+    _106 = sorted_by_key(_106)
+
+    if (len(not_in_106) != 0):
+        message_text = 'не нашел ' + otformatirovat(not_in_106) + ' в 106 группе)) Попробуйте написать фамилии по русски, желательно как в журнале Орлова.'
+        vk.messages.send(peer_id=Peer_id, message=message_text, random_id=time.time())
+        time.sleep(1)
+
+def distribution():
+    message_text = 'распределение:\n'
+
+    for problem in solved_problems:
+        solutions = sorted(solved_problems[problem], key=lambda x: _106[x])
+        unique_solutions = [i for i in solutions if i not in solved_something]
+
+        solver = unique_solutions[0] if len(unique_solutions) != 0 else solutions[0]
+
+        solved_something.add(solver)
+        message_text += problem + ' : @id' + str(find_id(solver)) + '(' + solver + ')' + '\n'
+
+    debug()
+    if (len(message_text) != 0):
+        vk.messages.send(peer_id=Peer_id, message=message_text, random_id=time.time())
+        time.sleep(1)
+
+def debug():
+    message_text = ''
+
+    for problem in solved_problems:
+        solutions = sorted(solved_problems[problem], key=lambda x: _106[x])
+        message_text += problem + ': ' + otformatirovat(solutions) + '\n\n'
+
+    if (len(message_text) != 0):
+        vk.messages.send(peer_id=Peer_id, message=message_text, random_id=time.time())
+    else:
+        vk.messages.send(peer_id=Peer_id, message='никто ничего не решил))', random_id=time.time())
+    time.sleep(1)
+
+def debug_2():
+    vk.messages.send(peer_id=Peer_id, message=otformatirovat2(_106), random_id=time.time())
+    time.sleep(1)
+
+def help(message):
+    documentation =  '''надо бы написать документацию'''
+
+    vk.messages.send(peer_id=Peer_id, message=documentation, random_id=time.time())
+    time.sleep(1)
+
+def stop_bot(message):
+    if (vk_map[message['from_id']] in admins):
+        exit(0)
+
+#--------------сам бот----------------
+
 _106 = read_106()
+
+if (len(sys.argv) > 1):
+    read_solved_problems()
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
         message = event.object.message
-        
-        if (message['from_id'] not in vk_map):
-            continue
-
         text = [i for i in message['text'].split(' ') if len(i) != 0]
 
-        if (text[0] == '\\distribution'):
-            answer_text = ''
+        if (len(text) == 0):
+            continue
 
-            for problem in solved_problems:
-                solutions = sorted(solved_problems[problem], key=lambda x: _106[x])
-                unique_solutions = [i for i in solutions if i not in solved_something]
-                try:
-                    solver = unique_solutions[0] if len(unique_solutions) != 0 else solutions[0]
-                except:
-                    solver = 'fucking nobody'
+        command = text[0]
+        print(command)
 
-                solved_something.add(solver)
-                answer_text += problem + ' : ' + solver + '\n'
+        if (command == '\\restart'):
+            restart(message)
 
-            vk.messages.send(peer_id=Peer_id, message=answer_text, random_id=time.time())
+        elif (command == '\\solved'):
+            solved(message)
 
-            solved_problems = dict()
-            solved_something = set()
-            _106 = read_106()
+        elif (command == '\\force_solved'):
+            solved(message, True)
 
-        elif (text[0] == '\\solved'):
-            problems = text[1:]
+        elif (command == '\\not_solved'):
+            not_solved(message)
 
-            for problem in problems:
-                if (problem not in solved_problems):
-                    solved_problems[problem] = list()
-                solved_problems[problem].append(vk_map[message['from_id']])
+        elif (command == '\\high_priority'):
+            increase_priority(message)
 
-        elif (text[0] == '\\not_solved'):
-            problems = text[1:]
+        elif (command == '\\distribution'):
+            distribution()
 
-            for problem in problems:
-                if (problem in solved_problems):
-                    solved_problems[problem].remove(vk_map[message['from_id']])
+        elif (command == '\\debug'):
+            debug()
 
-                    if (len(solved_problems[problem]) == 0):
-                        solved_problems.pop(problem, None)
+        elif (command == '\\debug_2'):
+            debug_2()
 
-        elif (text[0] == '\\important'):
-            problems = text[1:]
+        elif (command == '\\help'):
+            help(message)
 
-            for problem in problems:
-                if (problem not in solved_problems):
-                    solved_problems[problem] = list()
-
-        elif (text[0] == '\\debug'):
-            vk.messages.send(peer_id=Peer_id, message=dict_to_str(solved_problems), random_id=time.time())
-
-        elif (text[0] == '\\debug_2'):
-            vk.messages.send(peer_id=Peer_id, message=dict_to_str(_106), random_id=time.time())
-
-        elif (text[0] == '\\exit'):
-            exit(0)
+        elif (command == '\\exit'):
+            stop_bot(message)
